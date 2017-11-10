e4d74d64e61221aaef46a19c8e3bd56ed3e62d1b
Charlie Kenney <charlesc.kenney@gmail.com>
2017-11-10 10:40:33 -0500

    add express boilerplate

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('hi')
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import hpp from 'hpp'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import compression from 'compression'
+
+import routes from './routes'
+
+const PROD = process.env.NODE_ENV === 'production'
+const PORT = process.env.PORT || 3000
+
+// initalize express app
+const app = express()
+
+// mount middleware
+app
+	.use(helmet())
+	.use(hpp())
+	.use(compression())
+	.use(morgan(PROD ? 'combined' : 'dev'))
+	.use(bodyParser.json())
+	.use(bodyParser.urlencoded({ extended: true }))
+	.use(cors())
+
+// disable some headers
+app
+	.disable('x-powered-by')
+	.disable('etag')
+
+// mount routes
+app
+	.use('/game', routes.gameRoute)
+
+// listen for traffic
+app.listen(PORT)
+
+process.on('uncaughtException', err => console.error(err))
